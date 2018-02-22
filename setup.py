c8dbdacef231e40375f47c86077b4eff0190cb46
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2018-02-22 18:58:45 +0500

    Fixed(setup): added README parsing

--- setup.py
+++ setup.py
@@ -1,4 +1,13 @@
+# coding: utf-8
+import os
 from setuptools import setup
+
+
+def read(*paths):
+    """Build a file path from *paths* and return the contents."""
+    with open(os.path.join(*paths)) as f:
+        return f.read()
+
 
 setup(
     name='shell-retry',
@@ -9,6 +18,7 @@ setup(
     author='Oleg Strizhechenko',
     author_email='oleg.strizhechenko@gmail.com',
     description='Wrapper for call any utilities with retries until they succeed',
+    long_description=(read('README.md')),
     entry_points={
         'console_scripts': [
             'shell-retry=shell_retry.__init__:main',
